004084a4e0ad5ef6e162f4df5bb7b14156bf5369
Lauti14 <lautaro3314@hotmail.com>
2024-09-10 10:48:41 -0300

    update

---
+++ Prog3-Comision4/JS/tp javascript/main.js
@@ -0,0 +1,28 @@
+//1. Introducción a JavaScript
+
+/*
+Ejercicio 2:
+*/
+
+
+let a = 4;
+let b = 5;
+
+let c = a + b;
+console.log("La suma de a y b es: ", c);
+
+/*
+Ejercicio 3:
+*/
+
+/*
+let nombre = prompt("Ingrese su nombre: ");
+console.log("Hola, ", nombre, "!")
+*/
+
+//2.Operadores logicos y condicionales
+
+/*
+Ejercicio 1:
+*/
+
