5b9306f7c66608aa699b58b47f66523f9ee5af0c
Lauti14 <lautaro3314@hotmail.com>
2024-10-07 17:14:21 -0300

    app done

--- Proyecto_React_TypeScript/proyectoReact/src/components/AppProduct/FormProduct/FormProduct.tsx
+++ Proyecto_React_TypeScript/proyectoReact/src/components/AppProduct/FormProduct/FormProduct.tsx
@@ -1,14 +1,22 @@
-import { Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { useForm } from "../../../hooks/useForm";
+import { FC } from "react";
 
-export const FormProduct = () => {
+interface IPropsFormProduct {
+	handleAddProduct: Function;
+}
+
+export const FormProduct: FC<IPropsFormProduct> = ({ handleAddProduct }) => {
 	const { handleChange, values, resetForm } = useForm({
 		nombre: "",
 		imagen: "",
 		precio: 0,
 	});
 
-	const handleSubmitForm = () => {};
+	const handleSubmitForm = () => {
+		handleAddProduct(values);
+		resetForm();
+	};
 
 	return (
 		<Form className="p-4 border rounded m-3">
@@ -35,15 +43,19 @@ export const FormProduct = () => {
 			</Form.Group>
 
 			<Form.Group controlId="formNombre">
-				<Form.Label>Nombre</Form.Label>
+				<Form.Label>Precio</Form.Label>
 				<Form.Control
 					type="number"
 					name="precio"
-					placeholder="Ingrese precio producto"
+					placeholder="Ingrese precio del producto"
 					value={values.precio}
 					onChange={handleChange}
 				/>
 			</Form.Group>
+
+			<div className="d-flex justify-content-center mt-4">
+				<Button onClick={handleSubmitForm} variant="primary">Enviar Producto</Button>
+			</div>
 		</Form>
 	);
 };
