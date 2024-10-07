554553163e7e55762d99d77aaffd25c392baa790
Lauti14 <lautaro3314@hotmail.com>
2024-10-07 16:33:28 -0300

    form componentes added

--- Proyecto_React_TypeScript/proyectoReact/src/App.tsx
+++ Proyecto_React_TypeScript/proyectoReact/src/App.tsx
@@ -1,16 +1,20 @@
 import { ComponentCounter } from "./components/ComponentCounter/ComponentCounter";
+import { ComponentUseEffect } from "./components/ComponetUseEffect/ComponentUseEffect";
+import { FormComponent } from "./components/FormComponent/FormComponent";
 import { MiPrimerComponent } from "./components/MiPrimerComponent/MiPrimerComponent";
 
 export const App = () => {
 	return (
-		<div>
+		<div style={{ display: "flex", flexDirection: "column", gap: "2vh" }}>
 			<MiPrimerComponent
 				text={"Texto desde propiedades"}
 				color="red"
 				fontSize={3}
 			/>
 
-            <ComponentCounter/>
+			<ComponentCounter />
+			<ComponentUseEffect />
+			<FormComponent />
 		</div>
 	);
 };
---
+++ Proyecto_React_TypeScript/proyectoReact/src/components/ComponetUseEffect/ComponentUseEffect.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const ComponentUseEffect = () => {
+
+    const[state, setState] = useState(false)
+
+	
+    
+    useEffect(()=>{
+        console.log(state);
+    },[state])
+
+	return <div>
+        <p>{state ? "Es true" : "Es false"}</p>
+        <button onClick={()=>{setState(!state)}}>Cambiar state</button>
+    </div>;
+};
